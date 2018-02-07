a793005610f4e03ae9bc12ddec29a1bedc1f0321
wustor <wuster2014@gmail.com>
2018-02-07 14:39:35 +0800

    增加示例代码

--- app/src/main/java/com/wustor/httphelper/MainActivity.java
+++ app/src/main/java/com/wustor/httphelper/MainActivity.java
@@ -2,31 +2,35 @@ package com.wustor.httphelper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.wustor.httphelper.callback.StringCallback;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initRequest();
+        tvResult = (TextView) findViewById(R.id.tv_response);
     }
 
-    private void initRequest() {
-        String url = "https://10.16.17.43:9002/Interface/getGuidePage";
-        RequestManager requestManager = new RequestManager(url);
+
+    public void testGet(View view) {
+        String url = "http://gank.io/api/data/Android/10/1";
+        RequestManager requestManager = new RequestManager(url, RequestManager.RequestMethod.GET);
         requestManager.setCallback(new StringCallback() {
             @Override
             public void onSuccess(String result) {
-                Log.d("d---->", result);
+                tvResult.setText(result);
             }
 
             @Override
             public void onFailure(AppException e) {
-                Log.d("d---->", e.toString());
+                tvResult.setText(e.toString());
 
             }
         });
@@ -34,5 +38,23 @@ public class MainActivity extends AppCompatActivity {
         HttpHelper.getInstance().execute(this, requestManager);//发起请求
     }
 
+    public void testPost(View view) {
+        String url = "https://10.16.17.43:9002/Interface/getGuidePage";
+        RequestManager requestManager = new RequestManager(url);
+        requestManager.setCallback(new StringCallback() {
+            @Override
+            public void onSuccess(String result) {
+                tvResult.setText(result);
 
+            }
+
+            @Override
+            public void onFailure(AppException e) {
+                tvResult.setText(e.toString());
+
+            }
+        });
+
+        HttpHelper.getInstance().execute(this, requestManager);//发起请求
+    }
 }
